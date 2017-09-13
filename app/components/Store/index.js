16b18f9a464552c62cfd7ecf6462679622c31682
sarea <sareakk@gmail.com>
2017-09-13 13:00:48 +0200

    fix style main page

--- app/components/Store/index.js
+++ app/components/Store/index.js
@@ -13,12 +13,12 @@ function Store({ store, selectItem }) {
   return (
     <div className="my-3 p-1 store-container">
       <div
-        className=""
+        className="store-container"
         onClick={() => {
           selectItem(store);
         }}
       >
-        <img className="img--fluid store-image" src="https://source.unsplash.com/400x300/?water" alt="A generic" />
+        <img className="img--fluid store-image" src="https://source.unsplash.com/400x300/?building" alt="A generic" />
         <div className="store-detail ">
           <h3>DE BIJENKORF</h3>
           <h1>{store.name}</h1>
